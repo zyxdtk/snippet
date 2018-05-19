5eae495a25e3f5e5e6e458a6f91bfaead16fe958
zyxdtk <zyxdtk@foxmail.com>
2018-05-19 14:59:38 +0800

    Update SymmetricTree.scala

--- scala/binarytree/SymmetricTree.scala
+++ scala/binarytree/SymmetricTree.scala
@@ -30,3 +30,46 @@ object Solution {
         result
     }
 }
+
+
+// bfs 352 ms
+import scala.collection.mutable._
+object Solution {
+    def isSymmetric(root: TreeNode): Boolean = {
+        var result = true;
+        if (root != null) {
+            val st = Stack[TreeNode]()
+            result = visitChild(st, root.left, root.right)
+            while (!st.isEmpty && result) {
+                val left = st.pop()
+                val right = st.pop()
+                if (left.value != right.value){
+                    result = false;
+                } else {
+                    result = visitChild(st, left.left, right.right)
+                    if (result) {
+                        result = visitChild(st, left.right, right.left)
+                    }
+                }
+            }
+        }
+        result   
+    }
+    
+    def visitChild(st: Stack[TreeNode], left: TreeNode, right: TreeNode): Boolean = {
+        var result = true;
+        if (isEmpty(left) && isEmpty(right)) {
+            result = true
+        } else if (isEmpty(left) || isEmpty(right)) {
+            result = false
+        } else{
+            st.push(left)
+            st.push(right)
+        }
+        result
+    }
+    
+    def isEmpty(root: TreeNode): Boolean = {
+        root == null
+    }
+}
