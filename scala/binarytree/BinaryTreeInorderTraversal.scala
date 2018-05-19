29cd1703a4f5332c83870767ba8f9841bf7cec65
zyxdtk <zyxdtk@foxmail.com>
2018-05-19 11:17:08 +0800

    Create BinaryTreeInorderTraversal.scala

---
+++ scala/binarytree/BinaryTreeInorderTraversal.scala
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var _value: Int) {
+ *   var value: Int = _value
+ *   var left: TreeNode = null
+ *   var right: TreeNode = null
+ * }
+ */
+import scala.collection.mutable._
+object Solution {
+    def inorderTraversal(root: TreeNode): List[Int] = {
+        val trivial = ListBuffer[Int]()
+        val st = Stack[TreeNode]()
+        if (root != null) {
+            st.push(root)
+        }
+        while (!st.isEmpty) {
+            val node = st.pop()
+            if (node.right == null && node.left == null) {
+                trivial += node.value
+            } else {
+                if (node.right != null) {
+                    st.push(node.right)
+                    node.right = null
+                }
+                st.push(node)
+                if (node.left != null) {
+                    st.push(node.left)
+                    node.left = null
+                }
+            }
+        }
+        trivial.toList
+    }
+}
