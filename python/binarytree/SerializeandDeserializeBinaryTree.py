35225045920f2d55c92e2b6eb4d223b316237112
zyxdtk <zyxdtk@foxmail.com>
2018-05-24 01:20:55 +0800

    Create SerializeandDeserializeBinaryTree.py

---
+++ python/binarytree/SerializeandDeserializeBinaryTree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# 学习 http://bookshadow.com/weblog/2015/10/26/leetcode-serialize-and-deserialize-binary-tree/
+class Codec:
+
+    def serialize(self, root):
+        def tuplify(root):
+            return root and (root.val, tuplify(root.left), tuplify(root.right))
+        return json.dumps(tuplify(root))
+
+    def deserialize(self, data):
+        def detuplify(t):
+            if t:
+                root = TreeNode(t[0])
+                root.left = detuplify(t[1])
+                root.right = detuplify(t[2])
+                return root
+        return detuplify(json.loads(data))
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
