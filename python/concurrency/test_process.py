0ba9a7ccbddbb89f56641a84437989a7b3636f8b
liyong <liyong2014@xiaochuankeji.cn>
2020-09-09 20:51:43 +0800

    liyong

---
+++ python/concurrency/test_process.py
@@ -0,0 +1,11 @@
+from multiprocessing import Pool
+
+
+def f(x):
+    raise RuntimeError
+    return x * x
+
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        print(p.map(f, [1, 2, 3]))
